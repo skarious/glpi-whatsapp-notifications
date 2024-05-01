839f953621fea992aa0801819722fa29aaa98e34
Citra IT - Excelência em TI <91758384+CitraIT@users.noreply.github.com>
2024-05-01 18:35:02 -0300

    Update hook_notify_requester.php

--- hook_notify_requester.php
+++ hook_notify_requester.php
@@ -10,48 +10,46 @@ function plugin_whatsappnotification_item_add(CommonDBTM $item){
     $entity_id = $item->fields['entities_id'];
     $entity = new Entity();
     $entity->getFromDb($entity_id);
-    print_r($entity->fields['name']);
-    die();
 
-        $user_id = $item->getActorsForType(CommonITILActor::REQUESTER)[0]["items_id"];
-        $user = new User();
-        $user->getFromDB($user_id);
-        $user_mobile_phone = $user->fields["mobile"];
-        if($user_mobile_phone != ""){
-                $data = array(
-                    'number' => $user_mobile_phone,
-                    'body' => "Olá {$user->fields['name']}\n" .
-                "Esta é uma mensagem automática de que sua solicitação sobre \"{$item->fields['name']}\" foi registrada com número de protocolo " .
-                "{$item->fields['id']}\nConte sempre conosco.",
-                    'userId' => '',
-                    'queueId' => '',
-                    'sendSignature' => true,
-                    'closeTicket' => false
-                );
-
-        $headers = array(
-                        'Content-Type: application/json',
-                        'Authorization: Bearer ' . WHATSAPP_TOKEN
+    $user_id = $item->getActorsForType(CommonITILActor::REQUESTER)[0]["items_id"];
+    $user = new User();
+    $user->getFromDB($user_id);
+    $user_mobile_phone = $user->fields["mobile"];
+    if($user_mobile_phone != ""){
+        $data = array(
+            "number" => $user_mobile_phone,
+            "body" => "Olá {$user->fields['name']}\n" .
+            "Esta é uma mensagem automática de que sua solicitação sobre \"{$item->fields['name']}\" foi registrada com número de protocolo " .
+            "{$item->fields['id']}\nConte sempre conosco.",
+                'userId' => '',
+                'queueId' => '',
+                'sendSignature' => true,
+                'closeTicket' => false
         );
 
-                $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, URL_API_WHATSAPP);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_HEADER, 1);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        $headers = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . WHATSAPP_TOKEN
+        );
 
-            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-            $response = curl_exec($ch);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, URL_API_WHATSAPP);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        $response = curl_exec($ch);
         curl_close($ch);
-                $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-        }else{
-        // TODO: adicionar logica para notificar caso nao seja encontrado o telefone do usuario.
-        }
+    }else{
+    // TODO: adicionar logica para notificar caso nao seja encontrado o telefone do usuario.
+    }
 
 }
 
